cc0fb617de7afd24207fd3f9317f122058b3c7ba
Matleena Kankaanpää <104016919+matleek@users.noreply.github.com>
2023-11-27 00:14:21 +0200

    userdaoon lisätty metodi käyttäjätunnuksella poistamiselle

--- src/main/java/com/automaatio/model/database/UserDAO.java
+++ src/main/java/com/automaatio/model/database/UserDAO.java
@@ -67,7 +67,26 @@ public class UserDAO implements IDAO {
         } finally {
             em.close();
         }
+    }
 
+    public void deleteObject(String username) {
+        EntityManager em = MysqlDBJpaConn.getInstance();
+        em.getTransaction().begin();
+        try {
+            User user = em.find(User.class, username);
+            if (user != null) {
+                em.remove(user);
+                System.out.println("User " + username + " deleted");
+            } else {
+                throw new IllegalArgumentException("User with username  " + username + " was not found");
+            }
+            em.getTransaction().commit();
+        } catch (Exception e) {
+            em.getTransaction().rollback();
+            throw e;
+        } finally {
+            em.close();
+        }
     }
 
     @Override
