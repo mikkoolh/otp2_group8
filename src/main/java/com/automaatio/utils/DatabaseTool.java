f0b7b58041e219cb29f9c1d295e9c4a479087195
Matleena Kankaanpää <104016919+matleek@users.noreply.github.com>
2023-11-27 05:58:45 +0200

    Update DatabaseTool.java

--- src/main/java/com/automaatio/utils/DatabaseTool.java
+++ src/main/java/com/automaatio/utils/DatabaseTool.java
@@ -9,13 +9,13 @@ public class DatabaseTool {
     public static void resetWeekdays() {
         if (weekdayDAO.getAll().size() != 7) {
             weekdayDAO.deleteAll();
-            weekdayDAO.addObject(new Weekday("Monday", "Maanantai", "Monday_ru", "Monday_ar"));
-            weekdayDAO.addObject(new Weekday("Tuesday", "Tiistai", "Tuesday_ru", "Tuesday_ar"));
-            weekdayDAO.addObject(new Weekday("Wednesday", "Keskiviikko", "Wednesday_ru", "Wednesday_ar"));
-            weekdayDAO.addObject(new Weekday("Thursday", "Torstai", "Thursday_ru", "Thursday_ar"));
-            weekdayDAO.addObject(new Weekday("Friday", "Perjantai", "Friday_ru", "Friday_ar"));
-            weekdayDAO.addObject(new Weekday("Saturday", "Lauantai", "Saturday_ru", "Saturday_ar"));
-            weekdayDAO.addObject(new Weekday("Sunday", "Sunnuntai", "Sunday_ru", "Sunday_ar"));
+            weekdayDAO.addObject(new Weekday("Monday", "Maanantai", "понедельник", "الأثنين"));
+            weekdayDAO.addObject(new Weekday("Tuesday", "Tiistai", "вторник", "الثلاثاء"));
+            weekdayDAO.addObject(new Weekday("Wednesday", "Keskiviikko", "среда", "الأربعاء"));
+            weekdayDAO.addObject(new Weekday("Thursday", "Torstai", "четверг", "الخميس"));
+            weekdayDAO.addObject(new Weekday("Friday", "Perjantai", "пятниц", "الجمعه"));
+            weekdayDAO.addObject(new Weekday("Saturday", "Lauantai", "суббота", "السبت"));
+            weekdayDAO.addObject(new Weekday("Sunday", "Sunnuntai", "воскресенье", "الأحد"));
         }
     }
 }
