2c341d87f23db643e90ef73d20153dc2d391ee35
Mikko <104062512+mikkoolh@users.noreply.github.com>
2023-11-28 15:14:02 +0200

    Update AbstractTestFX.java for headless

--- src/test/java/testFX/AbstractTestFX.java
+++ src/test/java/testFX/AbstractTestFX.java
@@ -34,4 +34,15 @@ public abstract class AbstractTestFX extends ApplicationTest {
         release(new KeyCode[]{});
         release(new MouseButton[]{});
     }
+
+    @Override
+    public void init() throws Exception {
+        Platform.setImplicitExit(false);
+        FxToolkit.registerPrimaryStage();
+    }
+    
+    @Override
+    public void destroy() {
+        FxToolkit.cleanupStages();
+    }
 }
