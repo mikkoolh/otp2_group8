e137589181f64e66f6951623aebf10d60590f26d
Nik Noss <nnossenko@gmail.com>
2023-12-06 17:16:31 +0200

    JavaDoc done for /controller/mainpage/menu

--- src/main/java/com/automaatio/controller/mainpage/menu/Menu.java
+++ src/main/java/com/automaatio/controller/mainpage/menu/Menu.java
@@ -1,5 +1,16 @@
 package com.automaatio.controller.mainpage.menu;
 
+/**
+ * The Menu interface defines the contract for implementing menus in the HomeAutomation application's main page.
+ * The show() method is responsible for showing the menu.
+ *
+ * @author Mikko Hänninen, Elmo Erla, Nikita Nossenko, Matleena Kankaanpää
+ * @version 1.0
+ */
 public interface Menu {
+    /**
+     * Displays specific menu.
+     * Implementing classes define the behavior for showing the menu.
+     */
     void show();
 }
--- src/main/java/com/automaatio/controller/mainpage/menu/MenuPopupController.java
+++ src/main/java/com/automaatio/controller/mainpage/menu/MenuPopupController.java
@@ -15,6 +15,16 @@ import java.net.URL;
 import java.util.Locale;
 import java.util.ResourceBundle;
 
+/**
+ * The MenuPopupController class is responsible for managing the menu popup that allows users to select a profile picture.
+ * It implements the Initializable interface to handle initialization of the controller.
+ * The controller is associated with an FXML file representing the UI for selecting a profile picture.
+ * Users can choose from various predefined profile pictures, and the selected picture is updated in the user's profile.
+ * The class interacts with the UserDAO and the ProfileController to update the user's profile picture.
+ *
+ * @author Nikita Nossenko
+ * @version 1.0
+ */
 public class MenuPopupController implements Initializable{
     @FXML
     private ImageView male, female, anime, hacker, detective, defaultpic;
@@ -26,10 +36,19 @@ public class MenuPopupController implements Initializable{
 
     private ProfileController profileController;
 
+    /**
+     * Sets the associated ProfileController for communication between controllers.
+     *
+     * @param profileController The ProfileController instance.
+     */
     public void setProfileController(ProfileController profileController) {
         this.profileController = profileController;
     }
 
+    /**
+     * Handles the click action when the "Save" button is clicked.
+     * Updates the user's profile picture in the database and closes the popup.
+     */
     @FXML
     private void onSaveClick() {
         if (selectedPic != -1) {
@@ -44,12 +63,23 @@ public class MenuPopupController implements Initializable{
         }
     }
 
+    /**
+     * Handles the click action when the "Exit" button is clicked.
+     * Closes the popup without saving changes.
+     */
     @FXML
     private void onExitClick() {
         Stage stage = (Stage) saveBtn.getScene().getWindow();
         stage.close();
     }
 
+    /**
+     * Initializes the controller by setting images for the predefined profile pictures and
+     * configuring click event handlers for each image.
+     *
+     * @param location  The URL location of the FXML file.
+     * @param resources The ResourceBundle containing localized resources.
+     */
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         male.setImage(new Image("images/male.jpg"));
@@ -67,6 +97,11 @@ public class MenuPopupController implements Initializable{
         defaultpic.setOnMouseClicked(event -> selectedPic = 6);
     }
 
+    /**
+     * Handles the click action when an image is clicked, updating the selected picture accordingly.
+     *
+     * @param mouseEvent The MouseEvent triggered by the image click.
+     */
     public void onImageClick(javafx.scene.input.MouseEvent mouseEvent) {
         ImageView clickedImageView = (ImageView) mouseEvent.getSource();
 
